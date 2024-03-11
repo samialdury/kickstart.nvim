b7076283e7360005a7685274895c1c42e641fa7d
Sami Al-Dury <git@aldury.com>
2024-03-11 13:25:13 +0700

    feat: copilot

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -32,6 +32,58 @@ return {
     'nvim-pack/nvim-spectre',
   },
   {
+    'zbirenbaum/copilot.lua',
+    cmd = 'Copilot',
+    build = ':Copilot auth',
+    lazy = true,
+    event = 'InsertEnter',
+    config = function()
+      require('copilot').setup {
+        panel = {
+          enabled = true,
+          auto_refresh = false,
+          keymap = {
+            jump_prev = '[[',
+            jump_next = ']]',
+            accept = '<CR>',
+            refresh = 'gr',
+            open = '<M-CR>',
+          },
+          layout = {
+            position = 'bottom', -- | top | left | right
+            ratio = 0.4,
+          },
+        },
+        suggestion = {
+          enabled = true,
+          auto_trigger = true, -- This
+          debounce = 75,
+          keymap = {
+            accept = '<C-a>',
+            accept_word = false,
+            accept_line = false,
+            next = '<M-]>',
+            prev = '<M-[>',
+            dismiss = '<C-]>',
+          },
+        },
+        filetypes = {
+          yaml = false,
+          markdown = false,
+          help = false,
+          gitcommit = false,
+          gitrebase = false,
+          hgcommit = false,
+          svn = false,
+          cvs = false,
+          ['.'] = false,
+        },
+        copilot_node_command = 'node', -- Node.js version must be > 18.x
+        server_opts_overrides = {},
+      }
+    end,
+  },
+  {
     'ThePrimeagen/vim-be-good',
     lazy = false,
   },
